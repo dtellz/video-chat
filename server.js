dcabc75bba19bf64633b286ea281536b405867d3
Diego <dtellez95@gmail.com>
2022-01-04 12:40:48 +0000

    arq: fixed by back to commonjs

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-import express from 'express';
-import { Server } from 'http';
-import { Socket } from 'socket.io';
-import { v4 as uuidv4 } from 'uuid';
-
-
-
-const app = express();
-const server = Server(app);
-const io = Socket(server);
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs'); //we will use EJS to simplify client side
 app.use(express.static('public')) //express will pull client scripts from public folder
 
-app.get('/', (req, res) => res.redirect(`/${uuidv4()}`)); // creates a new chat room with a random uuid
+app.get('/', (req, res) => res.redirect(`/${uuidV4()}`)); // creates a new chat room with a random uuid
 
 app.get('/:room', (req, res) => res.render('room', { roomId: req.params.room })); // user can enter an existing room via path params
 
